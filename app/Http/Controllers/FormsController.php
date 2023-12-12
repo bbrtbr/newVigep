eab922ef51ff3944be383b6f37dc1a3d846bfb01
bbrtbr <brenobrito984@gmail.com>
2023-12-12 08:58:39 -0300

    alteracao permissoes forms, dashboard

--- app/Http/Controllers/FormsController.php
+++ app/Http/Controllers/FormsController.php
@@ -4,17 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Cases as ModelsCases;
+use Spatie\Permission\Exceptions\UnauthorizedException;
 use App\Models\Patients, App\Models\Addresses, App\Models\RabiesCases, App\Models\HealthUnits, App\Models\HealthWorkers;
 class FormsController extends Controller
 {
     public function forms()
     {
+        if (!auth()->user()->hasRole('Super Admin')) {
+            throw new UnauthorizedException('403', 'Oops! Você não tem a autorização necessária.');
+        }else{
         return view('vigep.dashVigepForms');
+        }
     }
     public function dashboard(){
         $patients = Patients::all();
         $cases = ModelsCases::paginate(10);
+        if (!auth()->user()->hasRole('Super Admin')) {
+            throw new UnauthorizedException('403', 'Oops! Você não tem a autorização necessária.');
+        }else{
         return view('vigep.dashVigep', ['patients' => $patients, 'cases' => $cases]);
+        }
     }
     public function verifyCPF(Request $request) {
         $cpf = $request->input('patient_cpf');
