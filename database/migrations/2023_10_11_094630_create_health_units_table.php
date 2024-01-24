495d568e1446cff3d06a309a44dfffe04eabfcd5
unknown <brenobrito984@gmail.com>
2024-01-24 09:34:03 -0300

    correcao migrate

--- database/migrations/2023_10_11_094630_create_health_units_table.php
+++ database/migrations/2023_10_11_094630_create_health_units_table.php
@@ -9,7 +9,7 @@ class CreateHealthUnitsTable extends Migration
     public function up()
     {
         Schema::create('health_units', function (Blueprint $table) {
-            $table->id('health_unit_code');
+            $table->id('health_unit_code')->primary();
             $table->string('health_unit_name', 255)->nullable();
             $table->string('health_unit_email', 255)->nullable();
 
