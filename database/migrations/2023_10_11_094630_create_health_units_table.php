20f30c67d58bfe7af772fb0de5cb112b7090c0d8
unknown <brenobrito984@gmail.com>
2024-01-24 09:43:46 -0300

    correcao migrate 2

--- database/migrations/2023_10_11_094630_create_health_units_table.php
+++ database/migrations/2023_10_11_094630_create_health_units_table.php
@@ -9,15 +9,11 @@ class CreateHealthUnitsTable extends Migration
     public function up()
     {
         Schema::create('health_units', function (Blueprint $table) {
-            $table->id(); // 'id' é a chave primária padrão
-            $table->string('health_unit_code');
+            $table->string('health_unit_code')->primary(); // health_unit_code como chave primária
             $table->string('health_unit_name', 255)->nullable();
             $table->string('health_unit_email', 255)->nullable();
-
             $table->timestamps();
-
-            $table->primary('health_unit_code'); // Definindo 'health_unit_code' como chave primária
-        }); 
+        });
     }
 
     public function down()
