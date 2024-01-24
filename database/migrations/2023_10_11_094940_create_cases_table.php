911bdfddd9f21a7c3832142f8baf576537dde30c
unknown <brenobrito984@gmail.com>
2024-01-24 09:54:21 -0300

    correcao migrate 5

--- database/migrations/2023_10_11_094940_create_cases_table.php
+++ database/migrations/2023_10_11_094940_create_cases_table.php
@@ -1,48 +1,48 @@
-    <?php
+<?php
 
-    use Illuminate\Database\Migrations\Migration;
-    use Illuminate\Database\Schema\Blueprint;
-    use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-    class CreateCasesTable extends Migration
+class CreateCasesTable extends Migration
+{
+    public function up()
     {
-        public function up()
-        {
-            Schema::create('cases', function (Blueprint $table) {
-                $table->id('case_id');
-                $table->string('case_type')->nullable();
-                $table->enum('case_status', ['Aprovado', 'Reprovado', 'Analise'])->nullable();
-                $table->string('case_disease', 255)->nullable();
-                $table->string('case_cid', 45)->nullable();
-                $table->date('case_notification_date')->nullable();
-                $table->date('case_attendance_date')->nullable();
-                $table->unsignedBigInteger('get_patient_id')->nullable();
-                $table->unsignedBigInteger('get_health_worker_id')->nullable();
-                $table->string('health_unit_code',255)->nullable();
-                $table->unsignedBigInteger('get_address_id')->nullable();
+        Schema::create('cases', function (Blueprint $table) {
+            $table->id('case_id');
+            $table->string('case_type')->nullable();
+            $table->enum('case_status', ['Aprovado', 'Reprovado', 'Analise'])->nullable();
+            $table->string('case_disease', 255)->nullable();
+            $table->string('case_cid', 45)->nullable();
+            $table->date('case_notification_date')->nullable();
+            $table->date('case_attendance_date')->nullable();
+            $table->unsignedBigInteger('get_patient_id')->nullable();
+            $table->unsignedBigInteger('get_health_worker_id')->nullable();
+            $table->string('health_unit_code', 255)->nullable();
+            $table->unsignedBigInteger('get_address_id')->nullable();
 
-                $table->foreign('get_patient_id')->references('patient_id')->on('patients')
-                    ->onUpdate('NO ACTION')
-                    ->onDelete('NO ACTION');
+            $table->foreign('get_patient_id')->references('patient_id')->on('patients')
+                ->onUpdate('NO ACTION')
+                ->onDelete('NO ACTION');
 
-                $table->foreign('get_health_worker_id')->references('health_worker_id')->on('health_workers')
-                    ->onUpdate('NO ACTION')
-                    ->onDelete('NO ACTION');
+            $table->foreign('get_health_worker_id')->references('health_worker_id')->on('health_workers')
+                ->onUpdate('NO ACTION')
+                ->onDelete('NO ACTION');
 
-                    $table->foreign('health_unit_code')->references('health_unit_code')->on('health_units')
-                    ->onUpdate('NO ACTION')
-                    ->onDelete('NO ACTION');
+            $table->foreign('health_unit_code')->references('health_unit_code')->on('health_units')
+                ->onUpdate('NO ACTION')
+                ->onDelete('NO ACTION');
 
-                $table->foreign('get_address_id')->references('address_id')->on('addresses')
-                    ->onUpdate('NO ACTION')
-                    ->onDelete('NO ACTION');
+            $table->foreign('get_address_id')->references('address_id')->on('addresses')
+                ->onUpdate('NO ACTION')
+                ->onDelete('NO ACTION');
 
-                $table->timestamps();
-            });
-        }
-
-        public function down()
-        {
-            Schema::dropIfExists('cases');
-        }
+            $table->timestamps();
+        });
     }
+
+    public function down()
+    {
+        Schema::dropIfExists('cases');
+    }
+}
--- database/migrations/2023_10_11_095234_create_health_employments_table.php
+++ database/migrations/2023_10_11_095234_create_health_employments_table.php
@@ -10,10 +10,10 @@ class CreateHealthEmploymentsTable extends Migration
     {
         Schema::create('health_employments', function (Blueprint $table) {
             $table->id('health_employment_id');
-            $table->unsignedBigInteger('get_health_unit_id')->nullable();
+            $table->string('health_unit_code', 255)->nullable();
             $table->unsignedBigInteger('get_health_worker_id')->nullable();
 
-            $table->foreign('get_health_unit_id')->references('health_unit_id')->on('health_units')
+            $table->foreign('health_unit_code')->references('health_unit_code')->on('health_units')
                 ->onUpdate('NO ACTION')
                 ->onDelete('NO ACTION');
 
